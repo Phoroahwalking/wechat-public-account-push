a3367881771815dc11bece1021f46873f71779dc
Phoroahwalking <118037254+Phoroahwalking@users.noreply.github.com>
2022-11-12 13:26:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,9 +22,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'wx22a43de36ed541ed',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '	oCZsjv_0PgmWyclsJOJEZ5YLfe79N34bbdphlAMnAZE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
